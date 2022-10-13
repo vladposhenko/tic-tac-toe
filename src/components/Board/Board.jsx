8cbae4d9ce6ddab8073e53292fefe9aa5a5026ac
vladposhenko <posenkov5@gmail.com>
2022-10-13 21:31:09 +0300

    123

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import Square from "../Square/Square";
+import {calculateWinner} from "../../utils";
+
+class Board extends Component {
+
+    state = {
+        squares: Array(9).fill(null),
+        xIsNext:true
+    }
+
+    handleClick (i) {
+        const squares = this.state.squares.slice()
+        squares[i] = this.state.xIsNext ? "X" : "O"
+        this.setState({
+            squares,
+            xIsNext: !this.state.xIsNext
+        })
+    }
+
+    renderSquare(i) {
+        return <Square value={this.state.squares[i]}
+                        onClick={() => this.handleClick(i)}
+        />;
+    }
+
+    render() {
+        const winner = calculateWinner(this.state.squares)
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Board;
